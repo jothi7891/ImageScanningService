263eb7ca963855d49e4a4314ee6bb6de5c0b650f
jothi-abundat <jothi@abundat.com>
2025-04-10 14:33:24 +0100

    changes to infra

--- main.tf
+++ main.tf
@@ -224,7 +224,7 @@ resource "aws_api_gateway_method" "images_options" {
 resource "aws_api_gateway_method_response" "options_method_response" {
   rest_api_id = aws_api_gateway_rest_api.image_scan_api.id
   resource_id = aws_api_gateway_resource.images.id
-  http_method = aws_api_gateway_method.options_method.http_method
+  http_method = aws_api_gateway_method.images_options.http_method
   status_code = 200
 
   response_parameters = {
