42e5dbbf6ab6766c92230356264f9a7e2b85fd82
jothi-abundat <jothi@abundat.com>
2025-04-11 20:18:44 +0100

    adding fe deploy bucket

--- infra/main.tf
+++ infra/main.tf
@@ -97,6 +97,52 @@ resource "aws_s3_bucket_versioning" "front_end_deploy_bucket" {
   }
 }
 
+# Enable static website hosting
+resource "aws_s3_bucket_website_configuration" "front_end_deploy_bucket" {
+  bucket = aws_s3_bucket.front_end_deploy_bucket.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+}
+
+# Allow public read access to all objects
+resource "aws_s3_bucket_policy" "allow_public_read" {
+  bucket = aws_s3_bucket.front_end_deploy_bucket.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.front_end_deploy_bucket.arn}/*"
+      }
+    ]
+  })
+}
+
+# Disable block public access (required for website hosting)
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket = aws_s3_bucket.front_end_deploy_bucket.id
+
+  block_public_acls       = false
+  ignore_public_acls      = false
+  block_public_policy     = false
+  restrict_public_buckets = false
+}
+
+# Output the website endpoint
+output "website_endpoint" {
+  value = aws_s3_bucket_website_configuration.front_end_deploy_bucket.website_endpoint
+}
+
+
 # dyanmo tables
 resource "aws_dynamodb_table" "image_results" {
   name         = var.image_results_table
