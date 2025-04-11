afc7bc779b2b0c2baf1820ad41abd06703b87a4e
jothi-abundat <jothi@abundat.com>
2025-04-11 12:33:50 +0100

    add changes to lambda role

--- infra/main.tf
+++ infra/main.tf
@@ -128,13 +128,22 @@ resource "aws_iam_role" "lambda_exec" {
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
-    Statement = [{
+    Statement = [
+      {
       Action = "sts:AssumeRole",
       Effect = "Allow",
       Principal = {
         Service = "lambda.amazonaws.com"
       }
-    }]
+    },
+      {
+        Action    = "sts:AssumeRole",
+        Effect    = "Allow",
+        Principal = {
+          Service = "apigateway.amazonaws.com"  
+        }
+      }
+    ]
   })
 }
 
@@ -156,6 +165,11 @@ resource "aws_iam_role_policy_attachment" "lambda_rekognition" {
 resource "aws_iam_role_policy_attachment" "lambda_logs" {
   role       = aws_iam_role.lambda_exec.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_api_gateway" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonAPIGatewayInvokeLambdaFunction" 
 }
 
 #Lambda for handling 
